b1172314cd6c3f1dc7b8d988629eda4e061171cb
Eike Martin Löhden <eike.loehden@ub.uni-marburg.de>
2023-09-13 16:27:31 +0200

    Add possibility for multiplications

--- calculate.jl
+++ calculate.jl
@@ -1,8 +1,17 @@
-println("Hello user! This is a fantastic calculation app! Feel free to use it.")
-println("Until know, it can only add numbers.", "\n")
+println("Hello user! This is a fantastic calculation app! Feel free to use it.", "\n")
+print("Please chose your operation (possible is mult and sum): ")
+form = readline()
+println()
 print("Enter the first number: ")
 n1 = parse(Float64, readline())
 print("Enter the second number: ")
 n2 = parse(Float64, readline())
-println("Your calculation is: ", n1, " + ", n2, " = ", (n1+n2))
 
+if form == "sum"
+	println("Your calculation is: ", n1, " + ", n2, " = ", (n1+n2))
+elseif form == "mult"
+	println("Your calculation is: ", n1, " x ", n2, " = ", (n1*n2))
+else
+	println("Your operation form was invalid! Sorry.")
+end
+println("\n", "Good bye")
